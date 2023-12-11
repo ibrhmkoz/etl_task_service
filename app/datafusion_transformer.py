4aafdeb554d5733f945964bd9bd4d4e80290bb57
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2023-12-11 21:40:44 +0300

    Finish datafusion_transformer.py

--- app/datafusion_transformer.py
+++ app/datafusion_transformer.py
@@ -1,18 +1,41 @@
 import pyarrow as pa
 
 
+def get_pyarrow_type(column_type):
+    type_mapping = {
+        'int16': pa.int16(),
+        'int32': pa.int32(),
+        'int64': pa.int64(),
+        'float32': pa.float32(),
+        'float64': pa.float64(),
+        'utf8': pa.string()
+    }
+    return type_mapping.get(column_type, pa.null())
+
+
+def convert_to_pyarrow_schema(source_schema):
+    return pa.schema(
+        [(list(column.keys())[0], get_pyarrow_type(list(column.values())[0])) for column in
+         source_schema]
+    )
+
+
 class DatafusionTransformer:
-    def __init__(self, sql_query, context, source_schema):
+    def __init__(self, context, sql_query, source_schema):
         self.sql_query = sql_query
         self.context = context
-        self.source_schema = source_schema
+        self.source_schema = convert_to_pyarrow_schema(source_schema)
 
     def transform(self, kafka_messages):
         # Convert messages to a format suitable for Datafusion
         rows = [[int(value) for value in msg.decode('utf-8').split(',')] for msg in kafka_messages]
-        columns = list(map(list, zip(*rows)))
+        columns = [pa.array(col) for col in zip(*rows)]  # Create PyArrow arrays for each column
         batch = pa.RecordBatch.from_arrays(columns, schema=self.source_schema)
 
-        self.context.register_record_batches("temp_table", [batch])
+        # Register the batch as a table in the Datafusion context
+        self.context.register_record_batches("temp_table", [[batch]])
 
-        return self.context.sql(self.sql_query).collect()
+        # Execute the SQL query against the registered table
+        result = self.context.sql(self.sql_query).collect()
+
+        return result
---
+++ app/test_datafusion_transformer.py
@@ -0,0 +1,42 @@
+import pyarrow as pa
+import datafusion
+
+from app.datafusion_transformer import DatafusionTransformer
+
+
+def test_datafusion_transformer():
+    # Sample data similar to what KafkaSource returns
+    kafka_messages = [
+        b'10,20,40,5',
+        b'2,3,7,22',
+    ]
+
+    source_schema = [
+        {
+            "column_0": "int32"
+        },
+        {
+            "column_1": "int32"
+        },
+        {
+            "column_2": "int32"
+        },
+        {
+            "column_3": "int32"
+        }
+    ]
+
+    # Create a Datafusion context
+    ctx = datafusion.SessionContext()
+
+    # Define an SQL query for transformation
+    sql_query = "SELECT column_0+column_1+column_2+column_3 FROM temp_table"
+
+    # Create DatafusionTransformer instance
+    transformer = DatafusionTransformer(ctx, sql_query, source_schema)
+
+    # Transform data
+    transformed_data = transformer.transform(kafka_messages)
+
+    # Assertions
+    assert transformed_data[0].column(0) == pa.array([75, 34], type=pa.int32())
