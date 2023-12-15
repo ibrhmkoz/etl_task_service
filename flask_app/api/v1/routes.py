147a14fb93a809786ac28dddcbc8f08d848ca557
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2023-12-16 01:15:28 +0300

    Introduce HATEOAS

--- flask_app/api/v1/routes.py
+++ flask_app/api/v1/routes.py
@@ -1,17 +1,51 @@
-from flask import Blueprint, request
+from celery.result import AsyncResult
+from flask import Blueprint, request, url_for, jsonify
 from flask_app.tasks import start_etl_task as start_etl_celery_task
 
 api = Blueprint('api', __name__)
 
 
+@api.route('/etl_task/<task_id>', methods=['GET'])
+def get_task_status(task_id):
+    task_result = AsyncResult(task_id)
+    response = {
+        "task_id": task_id,
+        "status": task_result.status
+    }
+    return jsonify(response)
+
+
 @api.route('/etl_task', methods=['POST'])
 def start_etl_task():
     task_request = request.get_json()
-    return start_etl_celery_task.delay(kwargs=task_request)
+    task = start_etl_celery_task.delay(task_request)
+
+    response = {
+        "content": "ETL task started successfully",
+        "_links": [
+            {
+                "href": url_for('api.get_task_status', task_id=task.id, _external=False),
+                "rel": "status",
+                "type": "GET"
+            },
+            {
+                "href": url_for('api.abort_etl_task', task_id=task.id, _external=False),
+                "rel": "abort",
+                "type": "DELETE"
+            },
+            {
+                "href": url_for('api.start_etl_task', _external=False),
+                "rel": "self",
+                "type": "POST"
+            }
+        ]
+    }
+    return jsonify(response), 202
 
 
 @api.route('/etl_task/<task_id>', methods=['DELETE'])
 def abort_etl_task(task_id):
     task = start_etl_celery_task.AsyncResult(task_id)
     task.abort()
-    return
+    response = {"content": "ETL task aborted successfully"}
+    return jsonify(response), 200
--- flask_app/tasks.py
+++ flask_app/tasks.py
@@ -11,29 +11,34 @@ KAFKA_SOURCE_TIMEOUT = 1.0
 KAFKA_BATCH_SIZE = 10
 
 
+# @shared_task(bind=True, base=AbortableTask)
+# def start_etl_task(self, source_config, sink_config, sink_table, source_table, sql):
+#     source = KafkaSource.from_consumer_config(
+#         consumer_config=source_config,
+#         topic=source_table["topic"],
+#         timeout=KAFKA_SOURCE_TIMEOUT,
+#         batch_size=KAFKA_BATCH_SIZE
+#     )
+#
+#     transformer = DataFusionTransformer.create_data_fusion_transformer(
+#         sql_query=sql,
+#         source_schema=source_table["schema"]
+#     )
+#
+#     sink = KafkaSink.from_producer_config(
+#         producer_config=sink_config["bootstrap.servers"],
+#         sink_topic=sink_table["topic"]
+#     )
+#
+#     etl_task_iteration = ETLTaskIteration(source=source, transformer=transformer, sink=sink)
+#
+#     def is_not_aborted():
+#         return not self.is_aborted()
+#
+#     callback_looper = CallbackLooper(callback=etl_task_iteration, so_long_as=is_not_aborted)
+#     callback_looper.start_loop()
+
+
 @shared_task(bind=True, base=AbortableTask)
-def start_etl_task(self, source_config, sink_config, sink_table, source_table, sql):
-    source = KafkaSource.from_consumer_config(
-        consumer_config=source_config,
-        topic=source_table["topic"],
-        timeout=KAFKA_SOURCE_TIMEOUT,
-        batch_size=KAFKA_BATCH_SIZE
-    )
-
-    transformer = DataFusionTransformer.create_data_fusion_transformer(
-        sql_query=sql,
-        source_schema=source_table["schema"]
-    )
-
-    sink = KafkaSink.from_producer_config(
-        producer_config=sink_config["bootstrap.servers"],
-        sink_topic=sink_table["topic"]
-    )
-
-    etl_task_iteration = ETLTaskIteration(source=source, transformer=transformer, sink=sink)
-
-    def is_not_aborted():
-        return not self.is_aborted()
-
-    callback_looper = CallbackLooper(callback=etl_task_iteration, so_long_as=is_not_aborted)
-    callback_looper.start_loop()
+def start_etl_task(self, etl_task):
+    print(etl_task)
