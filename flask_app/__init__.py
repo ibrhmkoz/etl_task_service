edafba43b04acf001e2181391c5db016ef2e18bd
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2023-12-17 00:08:59 +0300

    Write celery Dockerfiles and docker-compose.yml
    I've written docker-compose.yml to see if everything works holistically before transitioning to Kubernetes

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -8,8 +8,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         CELERY=dict(
-            broker_url="redis://localhost",
-            result_backend="redis://localhost",
+            broker_url="redis://redis",
+            result_backend="redis://redis",
             task_ignore_result=True,
         ),
     )
